f181b941f18bbfcc29be4eced3c019796f69e1fd
CKH <khchung30630@gmail.com>
2023-08-24 15:46:14 +0900

    state 변경함수로 제목 변경 숙제

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,18 +1,29 @@
+/*eslint-disable*/
 import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let [title, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
-  let post = '강남 우동 맛집';
+  let [title, titleChange] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [likes, likesUp] = useState(0);
+
+  function setTitle() {
+    titleChange(['여자코트 추천', '강남 우동맛집', '파이썬독학']);
+  }
+
+  function plusLikes(){
+    likesUp(likes+1);
+  }
+  
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 블로그</div>
       </div>
       <div className='list'>
-        <h4>{title[0]}</h4>
+        <h4>{title[0]} <span onClick={plusLikes}>👍</span> {likes} </h4>
         <p>2월 17일 발행</p>
+        <button onClick={setTitle}>제목 변경 버튼</button>
       </div>
       <div className='list'>
         <h4>{title[1]}</h4>
