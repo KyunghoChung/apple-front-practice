443e5b5915ea156a1a254998ab573738ca0b165e
CKH <khchung30630@gmail.com>
2023-08-29 20:01:10 +0900

    9번째 리액트 강의 map()함수

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -19,55 +19,38 @@ function App() {
       <div className='black-nav'>
         <div>개발 블로그</div>
       </div>
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{title[0]} <span onClick={()=>likesUp(likes + 1)}>👍</span> {likes} </h4>
         <p>2월 17일 발행</p>
-        {/* <button onClick={()=>{
-          titleChange(['여자코트 추천', '강남 우동맛집', '파이썬독학'])
-        }}>제목 변경 버튼</button> */}
-        <button onClick={()=>{
-          let titleCopy = [...title];
-          titleCopy[0] = '여자코트 추천';
-          titleChange(titleCopy);
-          // 0번째 요소를 골라서 수정해줬다
-          // 그러나 원본 조작은 추천X, 그래서 복사본을 만들어서 수정
-          // 배열은 배열 안의 값이 아닌 레퍼런스를 갖기 때문에
-          // 배열 자체를 복사하면 레퍼런스 값이 복사되기 때문에
-          // 요소를 골라서 수정해줘도 변경되지 않는다.
-          // 그래서 배열을 벗겼다가 다시 배열을 입히는 방식으로 복사하면
-          // 레퍼런스 값이 달라지기 때문에 요소를 골라서 수정하면
-          // 복사본은 요소의 값이 변경된 배열이 된다.
-          // 위와 같이 ...을 사용하여 배열을 벗기는 문법: spread operator
-          // array나 object를 복사할 때 많이 사용한다.
-        }}>제목 변경 버튼</button>
       </div>
       <div className='list'>
         <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        {/* 단순 반전 */}
+        단순 반전
         <h4 onClick={()=>setModal(!modal)}>{title[2]}</h4>
-        {/* 삼항 연산자 */}
-        <h4 onClick={()=>{modal == false ? setModal(true) : setModal(false)}}>{title[2]}</h4>   
-        {/* if/else문 */}
-        <h4 onClick={()=>{
-          if(modal == false){
-            setModal(true);
-          }
-          else{
-            setModal(false);
-          }
-          }}>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-      {/* <div>
-          <button onClick={() =>{titleChange(sortedTitle)}}>글 제목 가나다순으로 정렬</button>
-      </div> */}
       {
       modal == true ? <Modal/> : null
+      } */}
+      {/* map() 실습을 위해 위 코드 정리하다가 
+      주석처리문을 중첩하면 예기치 못한 문제가 발생할 수 있다는 것을
+      알게 되었음 0829
+      */}
+
+      {
+        title.map(function(a, i){
+          return (
+            <div className='list'>
+            <h4>{ title[i] }  <span onClick={()=>likesUp(likes + 1)}>👍</span> {likes}</h4>
+            <p>2월 17일 발행</p>
+            </div>
+          )
+        })
       }
-      <ModalArrow></ModalArrow>
+      {/* <ModalArrow></ModalArrow> */}
     </div>
   );
 }
@@ -82,12 +65,12 @@ function Modal(){
   );
 }
 
-let ModalArrow= () =>{
-  return(
-    <div className='modal'>
-      <h4>컴포넌트 arrow function</h4>
-      <p>실습</p>
-    </div>
-  );
-}
+// let ModalArrow= () =>{
+//   return(
+//     <div className='modal'>
+//       <h4>컴포넌트 arrow function</h4>
+//       <p>실습</p>
+//     </div>
+//   );
+// }
 export default App;
