4e92e0ef333fdcd83f635a3ef6166d7c3fe2cb8e
CKH <khchung30630@gmail.com>
2023-11-13 12:30:52 +0900

    export/import 문법으로 다른 js 파일 활용하기

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+// import bg from './img/bg-150x150.png';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -14,7 +18,28 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <Button variant="primary">Primary</Button>{' '}
+
+      <div className = 'main-bg'></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{ shoes[0].title }</h4>
+            <p>{ shoes[0].price }</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{ shoes[1].title }</h4>
+            <p>{ shoes[1].price }</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{ shoes[2].title }</h4>
+            <p>{ shoes[2].price }</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
